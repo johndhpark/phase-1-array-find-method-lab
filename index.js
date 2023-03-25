9387cda8f3a07c3b8159db46af271a78770f7fea
John DH. Park <john.dh.park@me.com>
2023-03-24 22:12:27 -0700

    complete array-find-method

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+
+function superbowlWin(records) {
+	const latest = records.find((records) => records.result === "W");
+
+	if (latest === undefined) return undefined;
+
+	return latest.year;
+}
